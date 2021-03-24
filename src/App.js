173c1fdfc7ff5674482eb97a43a58251f42d08df
balazsve <balazs.vizhanyo@gmail.com>
2021-03-24 08:27:10 +0100

    71

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import "./homepage.component";
-import HomePage from "./homepage.component";
+
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      <HomePage />s
+      <HomePage />
     </div>
   );
 }
