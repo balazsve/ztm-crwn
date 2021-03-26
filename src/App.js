312dddbbc77edae3b6b3177615ec1ccc657ae1cc
balazsve <balazs.vizhanyo@gmail.com>
2021-03-26 21:47:40 +0100

    78

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+// function App() {
+//   return (
+//     <div>
+//       <HomePage />
+//     </div>
+//   );
+// }
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
