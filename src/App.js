43ee0b889bcb6100b6386e8579b9e9b5cc96d6b6
Balazs Vizhanyo <balazsvizhanyo@Balazss-MacBook-Pro.local>
2021-04-25 09:02:06 +0200

    google and email sinn in

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from "./components/header/header.component";
 
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import React from "react";
 
 
@@ -25,8 +25,25 @@ class App extends React.Component {
 unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser : user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+
+          console.log(this.state)
+
+        });
+      } 
+
+      this.setState({currentUser: userAuth});
+      
     });
   }
 
